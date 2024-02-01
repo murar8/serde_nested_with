2e9e2088b9c049691572cd77b6530a6eb3907a48
Lorenzo Murarotto <lnzmrr@gmail.com>
2024-02-01 21:28:23 +0000

    feat: initial implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,117 @@
+use darling::FromField;
+use proc_macro::TokenStream;
+use quote::{quote, ToTokens};
+use std::collections::hash_map::DefaultHasher;
+use std::collections::HashMap;
+use std::hash::{Hash, Hasher};
 
+const ATTRIBUTE_NAME: &str = "serde_nested_with";
+
+#[derive(Debug, Clone, Hash, FromField)]
+#[darling(attributes(serde_nested_with))]
+struct Field {
+    ty: syn::Type,
+    substitute: syn::Path,
+    with: String,
+}
+
+impl Field {
+    fn module_name(&self) -> String {
+        let mut hasher = DefaultHasher::new();
+        self.substitute.hash(&mut hasher);
+        format!("__serde_nested_with_{}", hasher.finish())
+    }
+
+    fn get_generic_argument(&self) -> syn::Path {
+        let full_ty = self.ty.to_token_stream().to_string();
+        let sub_ty = self.substitute.to_token_stream().to_string();
+        let placeholder_idx = sub_ty.find('_').expect("placeholder not found");
+        let (prefix, suffix) = sub_ty.split_at(placeholder_idx);
+        let suffix = &suffix[1..];
+        let generic = full_ty.strip_prefix(prefix).unwrap().strip_suffix(suffix).unwrap();
+        syn::parse_str::<syn::Path>(generic).expect("failed to parse generic argument")
+    }
+
+    fn plug_generic_argument(&self, generic_argument: &str) -> syn::Path {
+        let ty = self.substitute.to_token_stream().to_string();
+        let ty = ty.replace('_', generic_argument);
+        syn::parse_str::<syn::Path>(&ty).expect("failed to parse generic argument")
+    }
+
+    fn plug_generic_argument_with_path(&self, generic_argument: &str) -> syn::Path {
+        let ty = self.substitute.to_token_stream().to_string();
+        let ty = ty.replacen('<', ":: <", 1);
+        let ty = ty.replace('_', generic_argument);
+        syn::parse_str::<syn::Path>(&ty).expect("failed to parse generic argument")
+    }
+}
+
+#[proc_macro_attribute]
+pub fn serde_nested_with(_: TokenStream, input: TokenStream) -> TokenStream {
+    let mut input = syn::parse_macro_input!(input as syn::ItemStruct);
+    let mut modules = HashMap::new();
+
+    for field in input.fields.iter_mut() {
+        let attrs = match Field::from_field(field) {
+            Ok(attrs) => attrs,
+            Err(_) => continue,
+        };
+        for attr in field.attrs.iter_mut() {
+            if let syn::Meta::List(ref mut list) = attr.meta {
+                if let Some(syn::PathSegment { ident, .. }) = list.path.segments.first_mut() {
+                    if ident == ATTRIBUTE_NAME {
+                        *ident = syn::parse_quote!(serde);
+                        let module_name = attrs.module_name();
+                        list.tokens = quote! { with = #module_name };
+                        modules.insert(module_name, attrs);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    let modules = modules.into_iter().map(|(module_name, attrs)| {
+        let module_name_id = syn::Ident::new(&module_name, proc_macro::Span::call_site().into());
+        let inner_module_name = &attrs.with;
+        let field_ty = &attrs.ty;
+        let generic_argument = attrs.get_generic_argument();
+        let wrapper_type = attrs.plug_generic_argument("Wrapper");
+        let wrapper_type_with_path = attrs.plug_generic_argument_with_path("Wrapper");
+
+        quote! {
+            mod #module_name_id {
+                use super::*;
+                use serde::{Serialize, Deserialize};
+
+                #[derive(serde::Serialize, serde::Deserialize)]
+                #[serde(transparent)]
+                struct Wrapper(#[serde(with=#inner_module_name)] #generic_argument);
+
+                pub fn serialize<S: serde::Serializer>(
+                    val: &#field_ty,
+                    serializer: S
+                ) -> std::result::Result<S::Ok, S::Error> {
+                    let val: &#wrapper_type = unsafe { std::mem::transmute(val) };
+                    val.serialize(serializer)
+                }
+
+                pub fn deserialize<'de, D>(
+                    deserializer: D
+                ) -> std::result::Result<#field_ty, D::Error>
+                where
+                    D: serde::Deserializer<'de>,
+                {
+                    let v = #wrapper_type_with_path::deserialize(deserializer)?;
+                    Ok(unsafe { std::mem::transmute(v) })
+                }
+            }
+        }
+    });
+
+    let output = quote! {
+        #(#modules)*
+        #input
+    };
+    output.into()
+}
---
+++ tests/tests.rs
@@ -0,0 +1,62 @@
+use serde::{Deserialize, Serialize};
+use serde_test::{assert_tokens, Token};
+use serde_with_nested::serde_nested_with;
+use std::collections::BTreeMap;
+use time::serde::rfc3339;
+use time::OffsetDateTime;
+
+#[serde_nested_with]
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct Foo {
+    #[serde(with = "rfc3339")]
+    pub bar0: OffsetDateTime,
+    #[serde_nested_with(substitute = "Option<_>", with = "rfc3339")]
+    pub bar1: Option<OffsetDateTime>,
+    #[serde_nested_with(substitute = "Option<Option<_>>", with = "rfc3339")]
+    pub bar2: Option<Option<OffsetDateTime>>,
+    #[serde_nested_with(substitute = "Option<BTreeMap<i32, _>>", with = "rfc3339")]
+    pub bar3: Option<BTreeMap<i32, OffsetDateTime>>,
+}
+
+#[test]
+fn test_ser_de() {
+    let item = Foo {
+        bar0: OffsetDateTime::from_unix_timestamp(1000000000).unwrap(),
+        bar1: OffsetDateTime::from_unix_timestamp(1000000000).ok(),
+        bar2: OffsetDateTime::from_unix_timestamp(1000000000).ok().into(),
+        bar3: {
+            let mut map = BTreeMap::new();
+            map.insert(1, OffsetDateTime::from_unix_timestamp(1000000000).unwrap());
+            map.insert(2, OffsetDateTime::from_unix_timestamp(2000000000).unwrap());
+            map.insert(3, OffsetDateTime::from_unix_timestamp(3000000000).unwrap());
+            Some(map)
+        },
+    };
+
+    assert_tokens(
+        &item,
+        &[
+            Token::Struct { name: "Foo", len: 4 }, //:w
+            Token::Str("bar0"),
+            Token::Str("2001-09-09T01:46:40Z"),
+            Token::Str("bar1"),
+            Token::Some,
+            Token::Str("2001-09-09T01:46:40Z"),
+            Token::Str("bar2"),
+            Token::Some,
+            Token::Some,
+            Token::Str("2001-09-09T01:46:40Z"),
+            Token::Str("bar3"),
+            Token::Some,
+            Token::Map { len: Some(3) },
+            Token::I32(1),
+            Token::Str("2001-09-09T01:46:40Z"),
+            Token::I32(2),
+            Token::Str("2033-05-18T03:33:20Z"),
+            Token::I32(3),
+            Token::Str("2065-01-24T05:20:00Z"),
+            Token::MapEnd,
+            Token::StructEnd,
+        ],
+    );
+}
